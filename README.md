66c7fffb998e951daec6390c334dd200fceb5072
mshokry <msn4eg@gmail.com>
2018-03-25 12:47:34 +0200

    Final

--- README.md
+++ README.md
@@ -1,45 +1,34 @@
-**Edit a file, create a new file, and clone from Bitbucket in under 2 minutes**
 
-When you're done, you can delete the content in this README and update the file with details for others getting started with your repository.
+## Refrances
 
-*We recommend that you open this README in another tab as you perform the tasks below. You can [watch our video](https://youtu.be/0ocf7u76WSo) for a full demo of all the steps in this tutorial. Open the video in a new tab to avoid leaving Bitbucket.*
+* By Mahmoud Shokry.
+* PIR motion detector sensor with Arduino nano in ultimate power saving mode 
+* (reducing power consumption almost by 87% (1.7mA) of normal operation consumption (13mA)) 
+* Thanks to:- http://playground.arduino.cc/Learning/ArduinoSleepCode  for information about sleep.h library and modes
+* http://www.kevindarrah.com/download/arduino_code/LowPowerVideo.ino 
+* for useful explanation of  "Low Power Arduino! Deep Sleep Tutorial" for bare arduino uno chip
+* http://www.atmel.com/images/atmel-8271-8-bit-avr-microcontroller-atmega48a-48pa-88a-88pa-168a-168pa-328-328p_datasheet_complete.pdf  // datasheet
+*
+* NOTE: when coming back from POWER-DOWN mode, it takes a bit
+*       until the system is functional at 100%!! (typically <1sec)
 
+--- 
+## Sleeping Modes helper function Hibernate  
+`
+ /*set_sleep_mode
+ * SLEEP_MODE_IDLE - the lowest power saving mode
+ * SLEEP_MODE_ADC
+ * SLEEP_MODE_PWR_SAVE
+ * SLEEP_MODE_STANDBY
+ * SLEEP_MODE_PWR_DOWN - the highest power saving mode
+ * "The Power-down mode saves the register contents but freezes the Oscillator, disabling all other chip functions 
+ * until the next interrupt or hardware reset."  text from ATMEGA328P datasheet
+ */
+ `
 ---
-
-## Edit a file
-
-You’ll start by editing this README file to learn how to edit a file in Bitbucket.
-
-1. Click **Source** on the left side.
-2. Click the README.md link from the list of files.
-3. Click the **Edit** button.
-4. Delete the following text: *Delete this line to make a change to the README from Bitbucket.*
-5. After making your change, click **Commit** and then **Commit** again in the dialog. The commit page will open and you’ll see the change you just made.
-6. Go back to the **Source** page.
-
+*
+**
+*Italic*
+**Bold**
 ---
-
-## Create a file
-
-Next, you’ll add a new file to this repository.
-
-1. Click the **New file** button at the top of the **Source** page.
-2. Give the file a filename of **contributors.txt**.
-3. Enter your name in the empty file space.
-4. Click **Commit** and then **Commit** again in the dialog.
-5. Go back to the **Source** page.
-
-Before you move on, go ahead and explore the repository. You've already seen the **Source** page, but check out the **Commits**, **Branches**, and **Settings** pages.
-
----
-
-## Clone a repository
-
-Use these steps to clone from SourceTree, our client for using the repository command-line free. Cloning allows you to work on your files locally. If you don't yet have SourceTree, [download and install first](https://www.sourcetreeapp.com/). If you prefer to clone from the command line, see [Clone a repository](https://confluence.atlassian.com/x/4whODQ).
-
-1. You’ll see the clone button under the **Source** heading. Click that button.
-2. Now click **Check out in SourceTree**. You may need to create a SourceTree account or log in.
-3. When you see the **Clone New** dialog in SourceTree, update the destination path and name if you’d like to and then click **Clone**.
-4. Open the directory you just created to see your repository’s files.
-
-Now that you're more familiar with your Bitbucket repository, go ahead and add a new file locally. You can [push your change back to Bitbucket with SourceTree](https://confluence.atlassian.com/x/iqyBMg), or you can [add, commit,](https://confluence.atlassian.com/x/8QhODQ) and [push from the command line](https://confluence.atlassian.com/x/NQ0zDQ).
+[name](link)
--- rat_ardu.ino
+++ rat_ardu.ino
@@ -1,12 +1,13 @@
-// PIR motion detector sensor with Arduino nano in ultimate power saving mode 
-//(reducing power consumption almost by 87% (1.7mA) of normal operation consumption (13mA)) 
-// Thanks to:- http://playground.arduino.cc/Learning/ArduinoSleepCode  for information about sleep.h library and modes
-// http://www.kevindarrah.com/download/arduino_code/LowPowerVideo.ino 
-// for useful explanation of  "Low Power Arduino! Deep Sleep Tutorial" for bare arduino uno chip
-// http://www.atmel.com/images/atmel-8271-8-bit-avr-microcontroller-atmega48a-48pa-88a-88pa-168a-168pa-328-328p_datasheet_complete.pdf  // datasheet
+/*
+* By Mahmoud Shokry.
+* check the MD file for ref 
+*/
 
 #include <avr/interrupt.h>        // Library to use interrupt
 #include <avr/sleep.h>            // Library for putting our arduino into sleep modes
+
+#include <avr/power.h>
+#include <avr/wdt.h>
 
 const int PIRsensorInterrupt = 0; //interrupt 0 at arduino nano pin D2
 const int PIRsensor = 2;          //interrupt 0 at arduino nano pin D2
@@ -24,42 +25,23 @@ const int esp_ack = 8;            // Done oeration and ready to sleep
 volatile int lastPIRsensorState = 1;  // previous sensor state
 volatile int PIRsensorState = 0;  // current state of the button
 
+volatile int wdt_wake=0;
+
 void wakeUpNow(){                 // Interrupt service routine or ISR  
   PIRsensorState = !lastPIRsensorState;    // we negate previous state and assign to current state
 }
 
-void setup() {
-  pinMode(LedPin, OUTPUT);        // initialize pin 13 as an output pin for LED or relay etc.
-  pinMode(PIRsensor, INPUT);      // define interrupt pin D2 as input to read interrupt received by PIR sensor
-  pinMode(btn_config, INPUT);
-  pinMode(btn_update, INPUT);
-  pinMode(btn_reset, INPUT);
-
-  pinMode(esp_wake, OUTPUT);
-  pinMode(esp_update, OUTPUT);
-  pinMode(esp_config, OUTPUT);
-  pinMode(esp_motion, OUTPUT);
-  pinMode(esp_ack, INPUT);
-
-  digitalWrite(LedPin, LOW);
-  digitalWrite(esp_motion, LOW);
-  digitalWrite(esp_config, LOW);
-  digitalWrite(esp_update, LOW);
-  digitalWrite(esp_wake, LOW);
-
-
-  //Serial.begin(115200);     // initialize serial communication only for debugging purpose
-  // Serial.println("Warming up... wait for a min...");
-
- // delay execution of sketch for a min, to allow PIR sensor get stabilized
- for( int i = 1; i <= 10; i++){  // LED at pin 13 blinks until PIR sensor is stabilized 10 seconds
-    delay(1000); 
- }
- 
- // Serial.println("Ready");     // enable only for debugging purpose
-  
-  
+// Watchdog Interrupt Service. This is executed when watchdog timed out.
+ISR(WDT_vect) {
+  if(wdt_wake == 0) {
+    // here we can implement a counter the can set the wdt_wake to true if
+    // the watchdog cycle needs to run longer than the maximum of eight
+    // seconds.
+    wdt_wake=1;
+  }
 }
+
+
 
 void Hibernate()         // here arduino is put to sleep/hibernation
 {
@@ -78,7 +60,7 @@ void Hibernate()         // here arduino is put to sleep/hibernation
  sleep_enable();                       // enable the sleep mode function
  sleep_bod_disable();                  //to disable the Brown Out Detector (BOD) before going to sleep. 
 
-   /*Now it is time to enable an interrupt. In the function call
+   /*
    * attachInterrupt(A, B, C)
    * A   can be either 0 or 1 for interrupts on pin 2 or 3.  
    *
@@ -111,6 +93,81 @@ void Hibernate()         // here arduino is put to sleep/hibernation
  detachInterrupt(PIRsensorInterrupt);   // we detach interrupt from pin D2, to avoid further interrupts until our ISR is finished
 }
 
+
+// Setup the Watch Dog Timer (WDT)
+void setupWatchDogTimer() {
+  // The MCU Status Register (MCUSR) is used to tell the cause of the last
+  // reset, such as brown-out reset, watchdog reset, etc.
+  // NOTE: for security reasons, there is a timed sequence for clearing the
+  // WDE and changing the time-out configuration. If you don't use this
+  // sequence properly, you'll get unexpected results.
+
+  // Clear the reset flag on the MCUSR, the WDRF bit (bit 3).
+  MCUSR &= ~(1<<WDRF);
+
+  // Configure the Watchdog timer Control Register (WDTCSR)
+  // The WDTCSR is used for configuring the time-out, mode of operation, etc
+
+  // In order to change WDE or the pre-scaler, we need to set WDCE (This will
+  // allow updates for 4 clock cycles).
+
+  // Set the WDCE bit (bit 4) and the WDE bit (bit 3) of the WDTCSR. The WDCE
+  // bit must be set in order to change WDE or the watchdog pre-scalers.
+  // Setting the WDCE bit will allow updates to the pre-scalers and WDE for 4
+  // clock cycles then it will be reset by hardware.
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+  /**
+   *  Setting the watchdog pre-scaler value with VCC = 5.0V and 16mHZ
+   *  WDP3 WDP2 WDP1 WDP0 | Number of WDT | Typical Time-out at Oscillator Cycles
+   *  0    0    0    0    |   2K cycles   | 16 ms
+   *  0    0    0    1    |   4K cycles   | 32 ms
+   *  0    0    1    0    |   8K cycles   | 64 ms
+   *  0    0    1    1    |  16K cycles   | 0.125 s
+   *  0    1    0    0    |  32K cycles   | 0.25 s
+   *  0    1    0    1    |  64K cycles   | 0.5 s
+   *  0    1    1    0    |  128K cycles  | 1.0 s
+   *  0    1    1    1    |  256K cycles  | 2.0 s
+   *  1    0    0    0    |  512K cycles  | 4.0 s
+   *  1    0    0    1    | 1024K cycles  | 8.0 s
+  */
+  WDTCSR  = (0<<WDP3) | (1<<WDP2) | (1<<WDP1) | (1<<WDP0);
+  // Enable the WD interrupt (note: no reset).
+  WDTCSR |= _BV(WDIE);
+}
+
+void setup() {
+  pinMode(LedPin, OUTPUT);        // initialize pin 13 as an output pin for LED or relay etc.
+  pinMode(PIRsensor, INPUT);      // define interrupt pin D2 as input to read interrupt received by PIR sensor
+  pinMode(btn_config, INPUT);
+  pinMode(btn_update, INPUT);
+  pinMode(btn_reset, INPUT);
+
+  pinMode(esp_wake, OUTPUT);
+  pinMode(esp_update, OUTPUT);
+  pinMode(esp_config, OUTPUT);
+  pinMode(esp_motion, OUTPUT);
+  pinMode(esp_ack, INPUT);
+
+  digitalWrite(LedPin, LOW);
+  digitalWrite(esp_motion, LOW);
+  digitalWrite(esp_config, LOW);
+  digitalWrite(esp_update, LOW);
+  digitalWrite(esp_wake, LOW);
+
+
+  //Serial.begin(115200);     // initialize serial communication only for debugging purpose
+  // Serial.println("Warming up... wait for a min...");
+
+ // delay execution of sketch for a min, to allow PIR sensor get stabilized
+ for( int i = 1; i <= 10; i++){  // LED at pin 13 blinks until PIR sensor is stabilized 10 seconds
+    delay(1000); 
+ }
+ 
+ // Serial.println("Ready");     // enable only for debugging purpose
+ setupWatchDogTimer(); 
+  
+}
 
 void loop() {
  interrupts();    // enable interrupts for Due and Nano V3
